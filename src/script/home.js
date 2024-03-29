d65dde957d120e522a826d6da4edbf2699bce790
BuildTools <agiusangelo13@gmail.com>
2024-03-29 16:46:47 +0100

    modiffication du script de la popup

--- src/script/home.js
+++ src/script/home.js
@@ -1,3 +1,4 @@
 document.querySelector("#close-popup").addEventListener("click", function(){
     document.querySelector(".popup-frame").style.display = "none";
+    document.querySelector("html").style.overflowY = "visible";
 });
